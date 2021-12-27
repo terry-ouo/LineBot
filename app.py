a3d7a6e5b3e02ab4510d3c73435452f865dce4b6
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2021-12-27 22:16:18 +0800

    ok

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-from __future__ import unicode_literals
-import os
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import configparser
-import random
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
+line_bot_api = LineBotApi('dbbCXNIvFeRtDbFWSx0Ba3neQTyfRl1b8c0iVeFNNYsjE4lTsSV+LnhNhGB+VjiaANDfyFHM5058Ny59l4TzE/1PASiSijDKPIlW5RKA/NTj6zhCZGm/8dXOGdzM8n/WTlKMejIrZYcPnsX/eqF9jAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('04fa00fd847749a6e3dbf463fa515371')
+
+
 @app.route("/")
-def index():
-    homepage = "<h1>林俊宏Python測試網頁</h1>"
+def home():
+    return 'home OK'
 
-line_bot_api = LineBotApi("OarzRlVTfeU6LQCmMln68L5kwkgCL12NAByh2kUoqSF5CxtiXxV79MQMbGXM2EjpANDfyFHM5058Ny59l4TzE/1PASiSijDKPIlW5RKA/NSz1CMFFumpr0F496I6zjbkOfDgAvEMRIs7onCDo3K15QdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("04fa00fd847749a6e3dbf463fa515371")
-
-
-# LINE 聊天機器人的基本資料
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
-handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
-
-
-# 接收 LINE 的資訊
-@app.route("/callback", methods=["POST"])
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -40,16 +34,20 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
-    return "OK"
+    return 'OK'
 
+
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= "我是電影機器人，您輸入的是：" + event.message.text + "。祝福您有個美好的一天！" ))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
     app.run()
+    
