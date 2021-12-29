be4e819ac699ce5aa0b12f010d86f36c0474d734
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2021-12-29 17:59:11 +0800

    ok

--- app.py
+++ app.py
@@ -9,16 +9,25 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials, firestore
+from flask import Flask, app, render_template,request
+from flask.helpers import url_for
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('dbbCXNIvFeRtDbFWSx0Ba3neQTyfRl1b8c0iVeFNNYsjE4lTsSV+LnhNhGB+VjiaANDfyFHM5058Ny59l4TzE/1PASiSijDKPIlW5RKA/NTj6zhCZGm/8dXOGdzM8n/WTlKMejIrZYcPnsX/eqF9jAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('04fa00fd847749a6e3dbf463fa515371')
 
+credits = credentials.Certificate("ServiceKey.json")
+firebase_admin.initialize_app(credits)
+db = firestore.client()
 
 @app.route("/")
-def home():
-    return 'home OK'
+def index():
+    home = "<a href=/spider>蜘蛛</a>"
+    return home
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -50,4 +59,26 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run()
+
+#網頁爬蟲
+@app.route("/spider")
+def spider():
+#網址
+    url = "https://tw.hjwzw.com/"
+    data = request.get(url)
+    data.encoding = "utf-8"
+    sp = BeautifulSoup(data.txt, "html.parser")
+    result = sp.find("title",)
     
+    for item in result:
+        doc={
+            "title" : item
+        }
+
+        doc_ref = db.collection("小說")
+        doc_ref.set(doc)
+
+    return "已經上傳完畢"
+
+if __name__ == "__main__":
+    app.run()
--- spider.py
+++ spider.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 from flask import Flask, app, render_template,request
 from flask.helpers import url_for
 
-credits = credentials.Certificate()
+credits = credentials.Certificate("ServiceKey.json")
 firebase_admin.initialize_app(credits)
 db = firestore.client()
 
@@ -21,4 +21,17 @@ def spider():
     data = request.get(url)
     data.encoding = "utf-8"
     sp = BeautifulSoup(data.txt, "html.parser")
+    result = sp.find("title",)
     
+    for item in result:
+        doc={
+            "title" : item
+        }
+
+        doc_ref = db.collection("小說")
+        doc_ref.set(doc)
+
+    return "已經上傳完畢"
+
+if __name__ == "__main__":
+    app.run()
