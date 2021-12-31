d78995867f4e7e5a160465ca05656d06399690fb
terry <s1092810@gm.pu.edu.tw>
2021-12-31 11:54:57 +0800

    ok

--- app.py
+++ app.py
@@ -57,9 +57,7 @@ def handle_message(event):
     message = event.message.text
     if (message[:4].upper() == "LIST") :
         res = novel_list()
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "OK"))
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text = res))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Final"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = res))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Fail"))
         
