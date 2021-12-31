be7b5fb9c49af533473baab2371ff9b903dfc18c
terry <s1092810@gm.pu.edu.tw>
2021-12-31 10:56:29 +0800

    good

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+from typing import Collection
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -12,6 +12,7 @@ from linebot.models import (
 from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials, firestore
+import requests
 from flask import Flask, app, render_template,request
 from flask.helpers import url_for
 
@@ -20,13 +21,14 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('dbbCXNIvFeRtDbFWSx0Ba3neQTyfRl1b8c0iVeFNNYsjE4lTsSV+LnhNhGB+VjiaANDfyFHM5058Ny59l4TzE/1PASiSijDKPIlW5RKA/NTj6zhCZGm/8dXOGdzM8n/WTlKMejIrZYcPnsX/eqF9jAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('04fa00fd847749a6e3dbf463fa515371')
 
-credits = credentials.Certificate("ServiceKey.json")
-firebase_admin.initialize_app(credits)
+cred = credentials.Certificate("ServiceKey.json")
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 @app.route("/")
 def index():
     home = "<a href=/spider>蜘蛛</a>"
+    home += "<a href=/try>try</a>"
     return home
 
 # 監聽所有來自 /callback 的 Post Request
@@ -52,33 +54,77 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = event.message.text
+    if (message[:4].upper() == "LIST") :
+        res = novel_list()
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = res))
+        
+
+def novel_list():
+    info =""
+    collection_ref = db.collection("小說")
+    for doc in collection_ref[:5]:
+        info += doc
+    return info
 
 
-if __name__ == "__main__":
-    app.run()
+def find_update(url):
+  dic = {"chapter" : None ,"time" : None}
+  data = requests.get(url)
+  data.encoding = "utf-8"
+  sp = BeautifulSoup(data.text, "html.parser")
+  results = sp.find_all("a")
+  for res in results:
+    title = res.get('title')
+    if title != None:
+        if "章節名" in title:
+            print(title)
+            info = title.split("章節名")[-1].split("更新時間")
+            chapter = info[0].strip()
+            update_time = info[1].strip()
+            dic = {"chapter" : chapter ,"time" : update_time}
+        
+            return dic
 
 #網頁爬蟲
 @app.route("/spider")
 def spider():
 #網址
-    url = "https://tw.hjwzw.com/"
-    data = request.get(url)
+    url = "https://tw.hjwzw.com/#"
+    data = requests.get(url)
     data.encoding = "utf-8"
-    sp = BeautifulSoup(data.txt, "html.parser")
-    result = sp.find("title",)
-    
-    for item in result:
-        doc={
-            "title" : item
-        }
-
-        doc_ref = db.collection("小說")
-        doc_ref.set(doc)
-
+    sp = BeautifulSoup(data.text, "html.parser")
+    results = sp.select("a")
+    doc={}
+    for result in results:
+        title = result.get("title")
+        if title != None:
+            if "小說名" in title:
+                book_link = "https://tw.hjwzw.com"+result.get("href")
+                part = title.split("作者:")
+                novel_name = part[0].replace("小說名:","")
+                novel_author = part[1].replace("TXT下載 手打","")
+                # 找最新章節
+                update = BeautifulSoup(data.text, "html.parser")
+                update = find_update(book_link)
+                doc={
+                    "book link" : book_link,
+                    "title" : novel_name,
+                    "author" : novel_author,
+                    "new chapter" : update["chapter"],
+                    "update time": update["time"]
+                }
+                doc_ref= db.collection("小說").document(result.get("href").replace("/","").replace("Book",""))
+                doc_ref.set(doc)
+                
+        
     return "已經上傳完畢"
+
+
+@app.route("/try")
+def try_page():
+    return "ok"
 
 if __name__ == "__main__":
     app.run()
+
