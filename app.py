2175fa09cd6a6fab8d83a13feb11eaba69d4570f
terry <s1092810@gm.pu.edu.tw>
2021-12-31 11:51:25 +0800

    ok

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def handle_message(event):
         res = novel_list()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "OK"))
         # line_bot_api.reply_message(event.reply_token, TextSendMessage(text = res))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Final"))
+        # line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Final"))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "Fail"))
         
