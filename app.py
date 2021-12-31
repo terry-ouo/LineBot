b29e2a3984a2eecc56eea69810252e4b82fe8499
terry <s1092810@gm.pu.edu.tw>
2021-12-31 11:09:36 +0800

    good

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "OK"))
     if (message[:4].upper() == "LIST") :
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "step1"))
         res = novel_list()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "OK"))
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = res))
