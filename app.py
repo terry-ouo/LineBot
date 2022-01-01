489d2ff00df69cb99ce89161fb1166ac49b526f2
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-01-02 00:58:44 +0800

    ok

--- app.py
+++ app.py
@@ -67,8 +67,7 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, image_message)
     elif message == "小遊戲":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="小遊戲:猜拳! \n請輸入數字 1. 布 2.剪刀 3.石頭"))
-        handle_message(event)
+        line_bot_api.reply_message(handle_message(event), TextSendMessage(text="小遊戲:猜拳! \n請輸入數字 1. 布 2.剪刀 3.石頭"))
         event = int(event.message.text)
         result = finger_guess_game_judge(finger_guess_game_player(event), finger_guess_game_pc())
         line_bot_api.reply_message(TextSendMessage(text="result"))
