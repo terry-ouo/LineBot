74ea9aeb6b8258cdb89130fe4ecec903c4641d3a
terry <s1092810@gm.pu.edu.tw>
2022-01-04 11:32:50 +0800

    ok

--- app.py
+++ app.py
@@ -85,10 +85,10 @@ def handle_message(event):
         result = game.finger_guess_game_judge(0)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
     elif message == "剪刀":
-        result = finger_guess_game_judge(1)
+        result = game.finger_guess_game_judge(1)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
     elif message == "石頭":
-        result = finger_guess_game_judge(2)
+        result = game.finger_guess_game_judge(2)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
     elif message[:4].upper() == "HELP":
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=config.help_information))
@@ -166,37 +166,37 @@ def spider():
 
 
 # 猜拳遊戲
-def finger_guess_game_player(event):
-    hand = ["paper", "剪刀", "石頭"]
-    player = hand[event - 1]
-    return player
-
-
-def finger_guess_game_pc():
-    choice = random.randrange(3)
-    hand = ["布", "剪刀", "石頭"]
-    pc = hand[choice]
-    return pc
-
-
-def finger_guess_game_judge(even):
-    player = finger_guess_game_player(even)
-    pc = finger_guess_game_pc()
-    if pc == player:
-        return pc + "\n\ntie!ಠ_ರೃ"
-    if pc == "布" and player == "剪刀":
-        return pc + "\n\nplayer win!(｡◝‿◜｡)"
-    elif pc == "布" and player == "石頭":
-        return pc + "\n\npc win!(;´Д`)×"
-    if pc == "剪刀" and player == "布":
-        return pc + "\n\npc win!⁽͑˙˚̀⚐˚́˙⁾̉"
-    elif pc == "剪刀" and player == "石頭":
-        return pc + "\n\nplayer win!(´︶｀)"
-    if pc == "石頭" and player == "paper":
-        return pc + "\n\nplayer win!(≧◡≦)"
-    elif pc == "石頭" and player == "剪刀":
-        return pc + "\n\npc win!(╯︵╰,)"
-
+# def finger_guess_game_player(event):
+#     hand = ["paper", "剪刀", "石頭"]
+#     player = hand[event - 1]
+#     return player
+#
+#
+# def finger_guess_game_pc():
+#     choice = random.randrange(3)
+#     hand = ["布", "剪刀", "石頭"]
+#     pc = hand[choice]
+#     return pc
+#
+#
+# def finger_guess_game_judge(even):
+#     player = finger_guess_game_player(even)
+#     pc = finger_guess_game_pc()
+#     if pc == player:
+#         return pc + "\n\ntie!ಠ_ರೃ"
+#     if pc == "布" and player == "剪刀":
+#         return pc + "\n\nplayer win!(｡◝‿◜｡)"
+#     elif pc == "布" and player == "石頭":
+#         return pc + "\n\npc win!(;´Д`)×"
+#     if pc == "剪刀" and player == "布":
+#         return pc + "\n\npc win!⁽͑˙˚̀⚐˚́˙⁾̉"
+#     elif pc == "剪刀" and player == "石頭":
+#         return pc + "\n\nplayer win!(´︶｀)"
+#     if pc == "石頭" and player == "paper":
+#         return pc + "\n\nplayer win!(≧◡≦)"
+#     elif pc == "石頭" and player == "剪刀":
+#         return pc + "\n\npc win!(╯︵╰,)"
+#
 
 if __name__ == "__main__":
     app.run()
