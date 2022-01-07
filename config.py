bbee056088ca631de19d4efb87516a2b8208cb23
EN <b5903612@gmail.com>
2022-01-07 16:16:59 +0800

    make it better

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ map = "https://chapel.pu.edu.tw/assets/images/map.jpg"
 providence = "https://mgt.pu.edu.tw/var/file/10/1010/pictures/347/m/mczh-tw700x700_large4611_662435007133.jpg"
 help_information ="若要觀看小說，請輸入小說\n若要玩小遊戲，請輸入小遊戲\n若要觀看下學期行事曆，請輸入行事曆\n若要觀看靜宜地圖，請輸入地圖\n若要觀看主顧聖母堂照片，請輸入主顧聖母堂\n若要觀看編輯者，請輸入editor\n輸入編輯者名稱會有意外收穫喔！"
 error = "沒有這個功能喔Σ(´д｀;) \n 相關功能請打 help \n 需要回報錯誤，或是有想提供的建議請打 report"
-can_editor = "創作者:\n    Terry\n\t    EN\n\t    BigBoy\n\t    阿旭\n\t    水水水\n\t    竣泓\n\t   Dorncy"
+can_editor = "創作者:\nTerry\nEN\nBigBoy\n阿旭\n水水水\n竣泓\nDorncy"
 report = "https://forms.gle/v2eiF5RemsYpWSyM9"
 
 table = [
